f6b93e4c11eea43c40b91530285ad680b2a9398f
anlikel <antongubenko2@gmail.com>
2025-10-18 09:50:03 +0300

    edit main

--- src/main/java/ru/gubenko/server1/Server1Application.java
+++ src/main/java/ru/gubenko/server1/Server1Application.java
@@ -3,6 +3,7 @@ package ru.gubenko.server1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//main application
 @SpringBootApplication
 public class Server1Application {
 
